1f762d464022f4afab2e7388d6f6fff4a6e3146f
ed ben <edwardbenedict92@gmail.com>
2021-04-13 14:41:35 +0300

    feat: add admin panel

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   Blog.find()
@@ -36,6 +37,61 @@ app.get("/", (req, res) => {
         blogger: "EdBen",
         blogs: result,
       });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/blog/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id)
+    .then((result) => {
+      res.render("blog", { blogger: "EdBen", blog: result, title: "Details" });
+    })
+    .catch((error) => {
+      res.status(404).render("404", { title: "404", blogger: "EdBen" });
+    });
+});
+
+app.get("/admin", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("admin", {
+        title: "Admin Page",
+        blogger: "EdBen",
+        blogs: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/admin/add", (req, res) => {
+  res.render("addnewblog", { title: "New Blog", blogger: "EdBen" });
+});
+
+app.post("/admin/add", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/admin");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/admin/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ link: "/admin" });
     })
     .catch((error) => {
       console.log(error);
